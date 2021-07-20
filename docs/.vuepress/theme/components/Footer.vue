a14be5ada851485579f6a19108549586c207c0a9
Calvin Lau <calvin@mona.co>
2021-07-20 10:21:41 +0800

    Update explorer link

--- docs/.vuepress/theme/components/Footer.vue
+++ docs/.vuepress/theme/components/Footer.vue
@@ -29,6 +29,11 @@
                 Faucet
               </a>
             </div>
+            <div class="link">
+              <a href="/community">
+                Community
+              </a>
+            </div>
           </div>
           <div class="column">
             <h3>Wallets</h3>
@@ -47,10 +52,10 @@
             </div>
           </div>
           <div class="column">
-            <h3>Community</h3>
+            <h3>Explorer</h3>
             <div class="link">
-              <a href="/community">
-                Community
+              <a href="https://cronos-explorer.crypto.org/">
+                Explorer
               </a>
             </div>
           </div>
