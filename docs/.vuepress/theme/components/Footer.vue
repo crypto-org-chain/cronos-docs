43b160213b26d7b6d0b1e958c0a74c2a398e8282
William Lau <william.lau@crypto.com>
2022-06-07 15:53:42 +0800

    Add linkedIn icon and link

--- docs/.vuepress/theme/components/Footer.vue
+++ docs/.vuepress/theme/components/Footer.vue
@@ -64,14 +64,14 @@
           <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/cronos_chain">
             <img :src="$withBase('/social/twitter.svg')" alt="twitter" />
           </a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/cronos-labs/">
+            <img :src="$withBase('/social/linkedin.svg')" alt="linkedin" />
+          </a>
           <!-- <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/CryptoComOfficial">
             <img :src="$withBase('/social/facebook.svg')" alt="facebook" />
           </a>
           <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/cryptocomofficial/">
             <img :src="$withBase('/social/instagram.svg')" alt="instagram" />
-          </a>
-          <a target="_blank" rel="noopener noreferrer" href="https://hk.linkedin.com/company/cryptocom">
-            <img :src="$withBase('/social/linkedin.svg')" alt="linkedin" />
           </a>
           <a target="_blank" rel="noopener noreferrer"
             href="https://www.youtube.com/channel/UCOMprzxakZOqmY23LYIawmg"
