676c11fcd09257300bf19ac6b729b698303d934c
kai zhang <kai.zhang@crypto.com>
2022-07-08 16:21:57 +0800

    Config best practises for node hosts

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -37,6 +37,10 @@ module.exports = {
             link: "/getting-started/local-devnet"
           },
           {
+            text: "Cronos node setup best practises",
+            link: "/getting-started/cronos-node-best-practises"
+          },          
+          {
             text: "Deploy Smart Contract at Cronos",
             link: "/getting-started/cronos-smart-contract"
           },
@@ -152,6 +156,7 @@ module.exports = {
         "cronos-testnet",
         "cronos-gbtesnet",        
         "local-devnet",
+        "cronos-node-best-practises",
         "security-checklist.md",
         "defi-practice",
         "cronos-smart-contract"
