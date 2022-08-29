13b7d38cc8557d21c5d3075f27d43cd62718e8ee
Kether <kether.chau@crypto.com>
2022-08-29 17:22:36 +0800

    Update the seraching bar

--- docs/.vuepress/theme/components/Navbar.vue
+++ docs/.vuepress/theme/components/Navbar.vue
@@ -1,98 +1,119 @@
 <template>
   <header class="navbar">
     <div class="inner">
-      <SidebarButton @toggle-sidebar="$emit('toggle-sidebar')"/>
-      <router-link
-        :to="$localePath"
-        class="home-link"
-      >
+      <SidebarButton @toggle-sidebar="$emit('toggle-sidebar')" />
+      <router-link :to="$localePath" class="home-link">
         <img
           class="logotype narrow-hide"
           v-if="$site.themeConfig.logo"
           :src="$withBase($site.themeConfig.logo)"
           :alt="$siteTitle"
-        >
+        />
         <img
           class="logoicon wide-hide"
           v-if="$site.themeConfig.logo"
           :src="$withBase('/logo-icon-white.svg')"
           :alt="$siteTitle"
-        >
+        />
         <span
           ref="siteName"
           class="site-name"
           v-if="$siteTitle && !$site.themeConfig.logo"
           :class="{ 'can-hide': $site.themeConfig.logo }"
-        >{{ $siteTitle }}</span>
+          >{{ $siteTitle }}</span
+        >
       </router-link>
 
       <div
         class="links"
-        :style="linksWrapMaxWidth ? {
-          'max-width': linksWrapMaxWidth + 'px'
-        } : {}"
+        :style="
+          linksWrapMaxWidth
+            ? {
+                'max-width': linksWrapMaxWidth + 'px',
+              }
+            : {}
+        "
       >
-        <NavLinks class="can-hide"/>
-        <AlgoliaSearchBox
-          v-if="isAlgoliaSearch"
-          :options="algolia"
-        />
-        <SearchBox
-          v-else-if="$site.themeConfig.search !== false && $page.frontmatter.search !== false"
-          @alignRight=true
-          :alignRight=true
-        />
+        <NavLinks class="can-hide" />
+        <AlgoliaSearchBox v-if="isAlgoliaSearch" :options="algolia" />
+        <div class="input-container">
+          <input
+            class="input"
+            type="text"
+            v-model="input"
+            placeholder="What is Cronos?"
+            v-on:keyup.enter="onEnter"
+          />
+          <span class="span">↩Enter</span>
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
-import AlgoliaSearchBox from '@AlgoliaSearchBox'
-import SearchBox from '@SearchBox'
-import SidebarButton from '@theme/components/SidebarButton.vue'
-import NavLinks from '@theme/components/NavLinks.vue'
+import AlgoliaSearchBox from "@AlgoliaSearchBox";
+import SearchBox from "@SearchBox";
+import SidebarButton from "@theme/components/SidebarButton.vue";
+import NavLinks from "@theme/components/NavLinks.vue";
+import { ref } from "vue";
 
 export default {
   components: { SidebarButton, NavLinks, SearchBox, AlgoliaSearchBox },
 
-  data () {
+  data() {
     return {
-      linksWrapMaxWidth: null
-    }
+      linksWrapMaxWidth: null,
+      input: ref(""),
+    };
   },
 
-  mounted () {
-    const MOBILE_DESKTOP_BREAKPOINT = 719 // refer to config.styl
-    const NAVBAR_VERTICAL_PADDING = parseInt(css(this.$el, 'paddingLeft')) + parseInt(css(this.$el, 'paddingRight'))
+  methods: {
+    onEnter: function () {
+      window.open(
+        `https://docs.cronos.org/getting-started/readme?q=${this.input}`,
+        "_blank"
+      );
+    },
+  },
+
+  mounted() {
+    const MOBILE_DESKTOP_BREAKPOINT = 719; // refer to config.styl
+    const NAVBAR_VERTICAL_PADDING =
+      parseInt(css(this.$el, "paddingLeft")) +
+      parseInt(css(this.$el, "paddingRight"));
     const handleLinksWrapWidth = () => {
       if (document.documentElement.clientWidth < MOBILE_DESKTOP_BREAKPOINT) {
-        this.linksWrapMaxWidth = null
+        this.linksWrapMaxWidth = null;
       } else {
-        this.linksWrapMaxWidth = this.$el.offsetWidth - NAVBAR_VERTICAL_PADDING
-          - (this.$refs.siteName && this.$refs.siteName.offsetWidth || 0)
+        this.linksWrapMaxWidth =
+          this.$el.offsetWidth -
+          NAVBAR_VERTICAL_PADDING -
+          ((this.$refs.siteName && this.$refs.siteName.offsetWidth) || 0);
       }
-    }
-    handleLinksWrapWidth()
-    window.addEventListener('resize', handleLinksWrapWidth, false)
+    };
+    handleLinksWrapWidth();
+    window.addEventListener("resize", handleLinksWrapWidth, false);
   },
 
   computed: {
-    algolia () {
-      return this.$themeLocaleConfig.algolia || this.$site.themeConfig.algolia || {}
+    algolia() {
+      return (
+        this.$themeLocaleConfig.algolia || this.$site.themeConfig.algolia || {}
+      );
     },
 
-    isAlgoliaSearch () {
-      return this.algolia && this.algolia.apiKey && this.algolia.indexName
-    }
-  }
-}
+    isAlgoliaSearch() {
+      return this.algolia && this.algolia.apiKey && this.algolia.indexName;
+    },
+  },
+};
 
-function css (el, property) {
+function css(el, property) {
   // NOTE: Known bug, will return 'auto' if style value is 'auto'
-  const win = el.ownerDocument.defaultView
+  const win = el.ownerDocument.defaultView;
   // null means not to return pseudo styles
-  return win.getComputedStyle(el, null)[property]
+  return win.getComputedStyle(el, null)[property];
 }
 </script>
 
@@ -144,6 +165,33 @@ $searchBoxInputColor = #0b1426
     position relative
     display flex
     color $headerTextColor
+
+    .input-container {
+      position: relative;
+      width: 100%;
+      margin: 0;
+
+      .input {
+        display: block;
+        height: 15px;
+        padding: 10px 45px;
+        background: white url("/search-icon.svg") no-repeat 15px center;
+        background-size: 15px 15px;
+        font-size: 16px;
+        border: none;
+        border-radius: 5px;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+          rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+      }
+
+      .span {
+        position: absolute;
+        right: 5px;
+        top: -2px;
+        color: grey;
+      }
+    }
+
     .search-box
       margin-right 0
       flex: 0 0 auto
